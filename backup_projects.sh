ef3bd870d406fa5809a36e53c93d3f05eea082d7
rayan <rayan13170@protonmail.com>
2024-02-06 22:27:21 +0100

    initial commit

---
+++ backup_projects.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Specify the path to your text file containing GitHub links
+file_path="full/path/of/your/repositories.txt"
+
+# Username of the GitHub account
+# not the email address
+USERNAME="YOUR_USERNAME"
+
+# Password authentication has been removed from GitHub, so you need to use a personal access token
+# Create it by following the instructions here: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
+
+# This token is used to clone private repositories, if you only want to backup
+# public repositories, you can set the token as an empty string
+TOKEN="GITHUB_TOKEN"
+
+# Check if the file exists
+if [ -f "$file_path" ]; then
+    # Loop through each line in the file
+    while IFS= read -r REPO_NAME; do
+        # Check if the folder exists
+        if [ -d "$REPO_NAME" ]; then
+            # If the folder exists, git pull
+            echo "-----------------------------"
+            echo "> Folder $REPO_NAME exists, pulling latest changes..."
+            echo "-----------------------------"
+            cd "$REPO_NAME"
+            git pull
+            cd ..
+        else
+            # If the folder doesn't exist, git clone
+            echo "-----------------------------"
+            echo "> Folder $REPO_NAME does not exist, cloning repository..."
+            echo "-----------------------------"
+            git clone https://$TOKEN@github.com/$USERNAME/$REPO_NAME.git
+        fi
+        
+    done < "$file_path"
+else
+    echo "File $file_path not found."
+fi
